c04fa373ed769143a7f69ae264980f53e21d971a
Sultonbek Rakhimov <sultonbek.rakhimov@gmail.com>
2024-01-21 11:46:47 +0500

    Finish specific prompt example

---
+++ src/PromptEngineering.N2.SpecificPrompt/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.SemanticKernel;
+
+// Create configurations
+var configurationBuilder = new ConfigurationBuilder();
+configurationBuilder.AddUserSecrets<Program>();
+var configuration = configurationBuilder.Build();
+
+// Create kernel builder
+var kernelBuilder = Kernel.CreateBuilder();
+
+// Add OpenAI connector
+kernelBuilder.AddOpenAIChatCompletion(modelId: "gpt-4", apiKey: configuration["OpenAiApiSettings:ApiKey"]!);
+
+// Build kernel
+var kernel = kernelBuilder.Build();
+
+// Create more specific prompt
+Console.Write("Your request : ");
+var request = Console.ReadLine();
+var prompt = $"What is the intent of this request? {request}. You can choose between CheckWeather, SendEmail, SetAlarm";
+
+// Display request intent
+Console.WriteLine(await kernel.InvokePromptAsync(prompt));
