e1d29c46832240dd5d784429fb98b99f2800ecd2
Sultonbek Rakhimov <sultonbek.rakhimov@gmail.com>
2024-01-22 13:45:33 +0500

    Finish handlebar template example

---
+++ src/PromptEngineering.N10.HandleBarTemplate/Program.cs
@@ -0,0 +1,173 @@
+﻿using System.Text.Json;
+using HandlebarsDotNet;
+using Microsoft.Extensions.Configuration;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.PromptTemplates.Handlebars;
+
+// Create configurations
+var configurationBuilder = new ConfigurationBuilder();
+configurationBuilder.AddUserSecrets<Program>();
+var configuration = configurationBuilder.Build();
+
+// Create kernel builder
+var kernelBuilder = Kernel.CreateBuilder();
+
+// Add OpenAI connector
+kernelBuilder.AddOpenAIChatCompletion(modelId: "gpt-4", apiKey: configuration["OpenAiApiSettings:ApiKey"]!);
+
+// Build kernel
+var kernel = kernelBuilder.Build();
+
+var templateConfig = new PromptTemplateConfig
+{
+    Template = """
+               
+                   <message role=""system"">
+                       Instructions: Provide the intent of the request using the following example format. Keep in mind that chat history has higher priority than
+                       examples. Bonus : You can double check reasoning before giving answer. Thanks!
+                       {{intentExample}}
+                   </message>
+                   <message role=""system"">
+                       Choices : You can choose between the following intents:
+                       {{intentChoices}}
+               
+                       Choose following intent as fallback option:
+                       {{fallbackIntent}}
+                   </message>
+               
+                   <message role=""system"">
+                       Examples - chat history has higher priority than examples
+               
+                       {{#each examples}}
+                            {{#each this}}
+                                <message role=""{{role}}"">{{content}}</message>
+                            {{/each}}
+                       {{/each}}
+                   </message>
+                   <message role=""system"">
+                       Chat history :
+               
+                       {{#each history}}
+                           <message role=""{{role}}"">{{content}}</message>
+                       {{/each}}
+                   </message>
+               
+                   <message role=""user"">{{request}}</message>
+                   <message role=""system"">Intent : </message>
+
+               """,
+    TemplateFormat = "handlebars",
+};
+
+// Create few shot examples
+var fewShotExamples = new List<ChatHistory>
+{
+    new(
+        new[]
+        {
+            new ChatMessageContent(AuthorRole.User, "Let's remind others about today's gathering"),
+            new ChatMessageContent(AuthorRole.System, "Intent"),
+            new ChatMessageContent(AuthorRole.Assistant, JsonSerializer.Serialize(new UserIntent(IntentType.SendEmail)))
+        }
+    ),
+
+    new(
+        new[]
+        {
+            new ChatMessageContent(AuthorRole.User, "Send summary of last month's meeting"),
+            new ChatMessageContent(AuthorRole.System, "Intent"),
+            new ChatMessageContent(AuthorRole.Assistant, JsonSerializer.Serialize(new UserIntent(IntentType.SendMessage)))
+        }
+    ),
+    new(
+        new[]
+        {
+            new ChatMessageContent(AuthorRole.User, "Why it suddenly feels so cold?"),
+            new ChatMessageContent(AuthorRole.System, "Intent"),
+            new ChatMessageContent(AuthorRole.Assistant, JsonSerializer.Serialize( new UserIntent(IntentType.CheckWeather)))
+        }
+    ),
+    new(
+        new[]
+        {
+            new ChatMessageContent(AuthorRole.User, "Should I wear a coat ?"),
+            new ChatMessageContent(AuthorRole.System, "Intent"),
+            new ChatMessageContent(AuthorRole.Assistant, JsonSerializer.Serialize( new UserIntent(IntentType.CheckWeather)))
+        }
+    ),
+    new(
+        new[]
+        {
+            new ChatMessageContent(AuthorRole.User, "Should I buy a coat ?"),
+            new ChatMessageContent(AuthorRole.System, "Intent"),
+            new ChatMessageContent(AuthorRole.Assistant, JsonSerializer.Serialize( new UserIntent(IntentType.Unknown)))
+        }
+    ),
+    new(
+        new[]
+        {
+            new ChatMessageContent(AuthorRole.User, "Did anyone replied to the email ?"),
+            new ChatMessageContent(AuthorRole.System, "Intent"),
+            new ChatMessageContent(AuthorRole.Assistant, JsonSerializer.Serialize( new UserIntent(IntentType.Unknown)))
+        }
+    )
+};
+
+// Create chat history
+var history = new ChatHistory(
+    new[]
+    {
+        new ChatMessageContent(AuthorRole.User, "I hate sending emails, no one ever reads them"),
+        new ChatMessageContent(AuthorRole.Assistant, "I'm sorry to hear that. Messages may be a better way to communicate.")
+    }
+);
+
+// Create more specific prompt with structured output
+Console.Write("Your request : ");
+var request = Console.ReadLine();
+
+var kernelArguments = new KernelArguments
+{
+    { "intentExample", new UserIntent(IntentType.Unknown) },
+    { "intentChoices", EnumExtensions.GetValuesAsString<IntentType>() },
+    { "fallbackIntent", new UserIntent(IntentType.Unknown) },
+    { "examples", fewShotExamples },
+    { "history", history },
+    { "request", request }
+};
+
+// Render template
+var templateFactory = new HandlebarsPromptTemplateFactory();
+var template = templateFactory.Create(templateConfig);
+var renderedPrompt = await template.RenderAsync(kernel, kernelArguments);
+
+var getIntentFunction = kernel.CreateFunctionFromPrompt(templateConfig, new HandlebarsPromptTemplateFactory());
+
+// Display request intent
+var intent = await kernel.InvokeAsync<string>(getIntentFunction, kernelArguments);
+var userIntent = JsonSerializer.Deserialize<UserIntent>(intent!);
+Console.WriteLine(userIntent!.Intent);
+
+public record UserIntent(IntentType Intent);
+
+public enum IntentType
+{
+    Unknown,
+    CheckWeather,
+    SendEmail,
+    SendMessage,
+}
+
+public static class EnumExtensions
+{
+    public static Dictionary<string, int> GetValuesAndNames<TEnum>() where TEnum : struct, Enum
+    {
+        return Enum.GetValues<TEnum>().ToDictionary(enumValue => enumValue.ToString(), enumValue => (int)(object)enumValue);
+    }
+
+    public static string GetValuesAsString<TEnum>() where TEnum : struct, Enum
+    {
+        return string.Join(", ", GetValuesAndNames<TEnum>().Select(value => $"{value.Key} = {value.Value}"));
+    }
+}
